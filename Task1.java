e5d500c2679382932f8848ba544814f7f8981000
Ramzes <sirius_2014@inbox.ru>
2020-10-23 23:45:20 +0300

    Update Task1.java

--- Task1.java
+++ Task1.java
@@ -15,40 +15,34 @@ import static java.util.stream.Collectors.toList;
 
 public class Task {
 
-  static String sortString(String str) {
-    char[] chars = str.toCharArray();
-    Arrays.sort(chars);
-    return new String(chars);
+  
+  public static List<Map.Entry<String, List<Integer>>> parseStringArray(String[] strArray){
+
+    List<Pair<String, String>> pairs = Arrays.stream(strArray).map(line -> Arrays.stream(line.split(",")).map(x -> {
+      char[] chars = x.toCharArray();
+      Arrays.sort(chars);
+      return new Pair<>(x, new String(chars));
+    }).collect(toList())).flatMap(Collection::stream).collect(toList());
+
+    Map<String, List<Integer>> indexes = new HashMap<>();
+
+    for(int i = 0; i < pairs.size(); i++){
+      indexes.computeIfAbsent(pairs.get(i).getValue(), c-> new ArrayList<>()).add(i);
+    }
+
+    return indexes.entrySet().stream().filter(x -> x.getValue().size() > 1).collect(toList());
   }
 
   public static void main(String[] args) throws IOException {
+      String[] array = new String[5];
+      array[0] = "qwe";
+      array[1] = "wqe";
+      array[2] = "qwee";
+      array[3] = "a";
+      array[4] = "a";
 
-    try (InputStreamReader inStream = new InputStreamReader(System.in);
-         BufferedReader buffer = new BufferedReader(inStream)) {
+    System.out.println(parseStringArray(array));
 
-      String line = null;
-
-      while ((line = buffer.readLine()) != null) {
-
-        String finalLine = line;
-        ArrayList<Pair<String, String>> pairs = new ArrayList<>(Arrays.stream(line.split(","))
-                .map(x -> new Pair<>(x, sortString(x)))
-                .collect(toList()));
-
-        Map<String, List<Integer>> indexes = new HashMap<>();
-
-        for(int i = 0; i < pairs.size(); i++){
-          indexes.computeIfAbsent(pairs.get(i).getValue(), c-> new ArrayList<>()).add(i);
-        }
-
-
-        System.out.println(indexes.entrySet().stream().filter(x -> x.getValue().size() > 1).collect(toList()));
-
-      }
-
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
   }
 
 }
